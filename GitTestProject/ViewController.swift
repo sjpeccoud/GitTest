23ff395f976179a7d95fd22020d0e159d1525302
Samuel Peccoud <samuel.peccoud@gmail.com>
2020-03-23 14:30:06 -0600

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Samuel Peccoud on 3/23/20.
+//  Copyright © 2020 Samuel Peccoud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
